49916780426e056b60f54c957160629a036a1159
ilkcandogan <ilkcandogan@protonmail.com>
2023-06-25 17:22:20 +0300

    Header bar yapıldı.

--- App.jsx
+++ App.jsx
@@ -1,11 +1,40 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+import HomeScreen from './screens/HomeScreen';
+
+const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <View>
-      <Text>Merhaba, Dünya!</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: 'red',
+          },
+          headerTitleStyle: {
+            color: 'white',
+          },
+          headerTitleAlign: 'center',
+          headerRight: () => (
+            <Ionicons
+              name="ios-add"
+              size={24}
+              color="white"
+              style={{ marginRight: 10 }}
+              onPress={() => {
+                // İkon butona tıklandığında yapılacak işlemler
+              }}
+            />
+          ),
+        }}
+      >
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
