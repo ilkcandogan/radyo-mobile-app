a163c5c023ff3a5d51840d59c99396ba00977860
ilkcandogan <ilkcandogan@protonmail.com>
2023-06-25 18:39:12 +0300

    Left drawer yapıldı.

--- App.jsx
+++ App.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
+import { View, TouchableOpacity } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-
 import HomeScreen from './screens/HomeScreen';
+import LeftPanel from './screens/LeftPanel';
 
-const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
+      <Drawer.Navigator
+        initialRouteName="Home"
+        drawerContent={props => <LeftPanel {...props} />}
+        screenOptions={({ navigation }) => ({
           headerStyle: {
             backgroundColor: 'red',
           },
@@ -19,23 +22,31 @@ const App = () => {
             color: 'white',
           },
           headerTitleAlign: 'center',
-          headerRight: () => (
-            <Ionicons
-              name="ios-add"
-              size={24}
-              color="white"
-              style={{ marginRight: 10 }}
-              onPress={() => {
-                // İkon butona tıklandığında yapılacak işlemler
-              }}
-            />
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => navigation.toggleDrawer()} style={styles.hamburgerIcon}>
+              <Ionicons name="ios-menu" size={24} color="white" />
+            </TouchableOpacity>
           ),
-        }}
+        })}
       >
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
+        <Drawer.Screen name="Home" component={HomeScreen} />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
+};
+
+const styles = {
+  hamburgerIcon: {
+    marginLeft: 15,
+  },
+  leftPanel: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    
+    height: '100%',
+    backgroundColor: '#f2f2f2',
+  },
 };
 
 export default App;
---
+++ screens/LeftPanel.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Entypo from 'react-native-vector-icons/Entypo';
+
+const styles = {
+  leftPanel: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    height: '100%',
+    padding: 15,
+  },
+  item: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  icon: {
+    marginRight: 10,
+  },
+  grayColor: {
+    color: 'gray'
+  },
+  blackColor: {
+    color: 'black'
+  }
+};
+
+const LeftPanel = () => {
+  const menuItems = [
+    {name: 'Facebook', icon: 'logo-facebook'},
+    {name: 'Twitter', icon: 'logo-twitter'},
+    {name: 'Instagram', icon: 'logo-instagram'}
+    // Add more items here
+  ];
+
+  return (
+    <View style={styles.leftPanel}>
+      <TouchableOpacity style={styles.item}>
+        <Entypo name="folder-video" size={24} style={[styles.icon, styles.blackColor]} />
+        <Text style={styles.grayColor}>İstek Yap</Text>
+      </TouchableOpacity>
+      {menuItems.map((item, index) => (
+        <TouchableOpacity key={index} style={styles.item}>
+          <Ionicons name={item.icon} size={24} style={[styles.icon, styles.blackColor]} />
+          <Text style={styles.grayColor}>{item.name}</Text>
+        </TouchableOpacity>
+      ))}
+    </View>
+  );
+};
+
+export default LeftPanel;
