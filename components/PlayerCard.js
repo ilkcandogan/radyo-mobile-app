4449a23cff80b7fb68a4e911d8c2585b5a47d916
ilkcandogan <ilkcandogan@protonmail.com>
2023-06-26 23:45:08 +0300

    Radyo akış durdurma ve çalıştırma özellikleri geliştirildi.

--- components/PlayerCard.js
+++ components/PlayerCard.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, StyleSheet, TouchableOpacity } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import TrackPlayer from 'react-native-track-player';
 
 const PlayerCard = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  useEffect(() => {
+    setupPlayer();
+  }, []);
+
+  const setupPlayer = async () => {
+    await TrackPlayer.setupPlayer();
+    await TrackPlayer.add({
+      id: 'radioStream1',
+      url: 'https://radyo1.radyo-dinle.tc/8076/stream',
+      title: 'Radyo Yayını',
+      artist: 'Radyo İstasyonu',
+    });
+  };
+
+  const togglePlayback = async () => {
+    if (isPlaying) {
+      await TrackPlayer.pause();
+    } else {
+      await TrackPlayer.play();
+    }
+    setIsPlaying(!isPlaying);
+  };
+
   return (
     <View style={styles.container}>
-      <TouchableOpacity>
-        <Ionicons name="play-circle-outline" size={55} color="white" />
+      <TouchableOpacity onPress={togglePlayback}>
+        <Ionicons name={isPlaying? 'pause-circle-outline': "play-circle-outline"} size={55} color="white" />
       </TouchableOpacity>
     </View>
   );
